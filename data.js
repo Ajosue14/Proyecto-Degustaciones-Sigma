f522156dd088131aa58d9e82c5582ddd212b805e
Ajosue14 <alvaro.ortega.gar@icloud.com>
2026-04-10 15:09:24 -0500

    Create data.js

---
+++ data.js
@@ -0,0 +1,19 @@
+const stores = [
+  {
+    nombre: "PLAZA VEA MIRAFLORES",
+    codigos: "6240312/6245307",
+    lat: -12.112264,
+    lng: -77.029972,
+    dia: "Lunes",
+    camion: "F8X-899"
+  },
+  {
+    nombre: "METRO CHORRILLOS",
+    codigos: "6245178/6240486",
+    lat: -12.170642,
+    lng: -77.012518,
+    dia: "Martes",
+    camion: "F1G-866"
+  }
+];
+``
