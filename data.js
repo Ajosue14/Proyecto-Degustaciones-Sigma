0c8cd85658958d0bc023432d58a6a8181cc4871c
Ajosue14 <alvaro.ortega.gar@icloud.com>
2026-04-10 15:27:30 -0500

    Update data.js

--- data.js
+++ data.js
@@ -8,12 +8,52 @@ const stores = [
     camion: "F8X-899"
   },
   {
+    nombre: "METRO MIRAFLORES - SHELL",
+    codigos: "6240362/6245198",
+    lat: -12.122594,
+    lng: -77.029846,
+    dia: "Lunes",
+    camion: "F8X-899"
+  },
+  {
+    nombre: "WONG BAJADA BALTA",
+    codigos: "6241949/6245215",
+    lat: -12.122811,
+    lng: -77.032052,
+    dia: "Lunes",
+    camion: "F8X-899"
+  },
+  {
+    nombre: "METRO BARRANCO",
+    codigos: "6240354/6245199",
+    lat: -12.146251,
+    lng: -77.021723,
+    dia: "Lunes",
+    camion: "F8X-899"
+  },
+  {
+    nombre: "PLAZA VEA MIRAFLORES",
+    codigos: "6240312/6245307",
+    lat: -12.112264,
+    lng: -77.029972,
+    dia: "Lunes",
+    camion: "F8X-899"
+  },
+  {
+    nombre: "MAKRO CHORRILLOS (HUAYLAS)",
+    codigos: "6247651/6247643",
+    lat: -12.174321,
+    lng: -77.019302,
+    dia: "Lunes",
+    camion: "F8X-899"
+  },
+  {
     nombre: "METRO CHORRILLOS",
     codigos: "6245178/6240486",
     lat: -12.170642,
     lng: -77.012518,
-    dia: "Martes",
-    camion: "F1G-866"
+    dia: "Lunes",
+    camion: "F8X-899"
   }
 ];
 ``
